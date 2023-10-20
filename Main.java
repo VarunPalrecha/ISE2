abe94e3d8b53dc7d26a0b7481bcf7b42656d437b
Varun Palrecha <87081105+VarunPalrecha@users.noreply.github.com>
2023-10-20 15:57:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
